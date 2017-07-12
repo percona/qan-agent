4cbf68f052f641043658402310b93b533be723d5
Kamil Dziedzic <arvenil@klecza.pl>
2017-07-12 18:36:10 +0200

    let's try with docker

--- .travis-install-mysql.sh
+++ .travis-install-mysql.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-MYSQL_APT_CONFIG="mysql-apt-config_0.8.6-1_all.deb"
 
-echo "Installing MySQL ${MYSQL_VERSION}..."
-echo mysql-apt-config mysql-apt-config/select-server select mysql-${MYSQL_VERSION} | sudo debconf-set-selections
-wget http://dev.mysql.com/get/${MYSQL_APT_CONFIG}
-sudo dpkg --install ${MYSQL_APT_CONFIG}
-sudo apt-get update -q
-sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
-sudo mysql_upgrade
+docker run \
+  --name mysql \
+  -p ${MYSQL_HOST}:${MYSQL_PORT}:3306 \
+  -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
+  -d \
+  mysql:${MYSQL_VERSION}
