f2cd50dc81e8abb176813c2c0f3f1bc7883474db
Kamil Dziedzic <arvenil@klecza.pl>
2017-08-16 17:07:29 +0200

    semver treats version with "-" as pre-release and so it ignores it

--- test/version/version.go
+++ test/version/version.go
@@ -1,19 +1,29 @@
 package version
 
 import (
+	"strings"
+
 	"github.com/Masterminds/semver"
 )
 
 func Constraint(constraint, version string) (bool, error) {
+	// Drop everything after first dash.
+	// Version with dash is considered a pre-release
+	// but some MongoDB builds add additional information after dash
+	// even though it's not considered a pre-release but a release.
+	s := strings.SplitN(version, "-", 2)
+	version = s[0]
+
+	// Create new version
 	v, err := semver.NewVersion(version)
 	if err != nil {
 		return false, err
 	}
 
+	// Check if version matches constraint
 	constraints, err := semver.NewConstraint(constraint)
 	if err != nil {
 		return false, err
 	}
 	return constraints.Check(v), nil
-
 }
