8c5e3a15d49cbc22cdb91258c4ecbff894891404
Kamil Dziedzic <arvenil@klecza.pl>
2018-05-04 12:41:37 +0200

    Don't double sleep time.

--- with_backoff.sh
+++ with_backoff.sh
@@ -2,13 +2,12 @@
 
 # Retries a command a configurable number of times with backoff.
 #
-# The retry count is given by ATTEMPTS (default 5), the initial backoff
+# The retry count is given by ATTEMPTS (default 120), the backoff
 # timeout is given by TIMEOUT in seconds (default 1.)
 #
-# Successive backoffs double the timeout.
-# Source: https://stackoverflow.com/a/8351489/916440
+# Based on: https://stackoverflow.com/a/8351489/916440
 function with_backoff {
-  local max_attempts=${ATTEMPTS-5}
+  local max_attempts=${ATTEMPTS-120}
   local timeout=${TIMEOUT-1}
   local attempt=1
   local exitCode=0
@@ -25,7 +24,6 @@ function with_backoff {
     echo "Failure! Retrying in $timeout.." 1>&2
     sleep $timeout
     attempt=$(( attempt + 1 ))
-    timeout=$(( timeout * 2 ))
   done
 
   if [[ $exitCode != 0 ]]
